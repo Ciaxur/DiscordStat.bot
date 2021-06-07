d8c7e6d6d30b5b46e737ea627261bbaec3e77b71
Omar Omar <oomar@umich.edu>
2021-06-07 08:59:46 -0400

    ✨ Improve Cache

--- src/Commands/Cache.ts
+++ src/Commands/Cache.ts
@@ -17,20 +17,32 @@ export const UPTIME_CACHE_EXPIRE = 1 * 60 * 1000;   // 1 Minute
 
 /**
  * Keeps cache constrained to a given size limit
+ *  and removes stale entries
  * @param cache Cache to constrain
  * @param entriesLimit Limit of cached entries
  */
 export async function restrictUptimeCache(cache: IUptimeCache, entriesLimit: number) {
   const cachedEntries = Object.entries(cache);
+  const date_now = Date.now();
 
   // Early return, no need to do anything. Size has not been reached
   if (cachedEntries.length < entriesLimit || entriesLimit < 1) return;
 
+  // Clear oldest Entry & Expired Entries
   let oldestEntry = cachedEntries[0];
+  const expiredKeys = [];
   for (const [key, val] of cachedEntries) {
     if (val.timestamp < oldestEntry[1].timestamp)
       oldestEntry = [key, val];
+    else if (val.timestamp.getTime() + UPTIME_CACHE_EXPIRE > date_now)
+      expiredKeys.push(key);
   }
+  
   console.log(`Cleared Cache entry '${oldestEntry[0]}'. Limited to ${entriesLimit}`);
   delete cache[oldestEntry[0]];
+
+  for (const key of expiredKeys) {
+    console.log(`Cleared Expired Cache entry '${key}'`);
+    delete cache[key];
+  }
 }
