79b416650c3112b0e8bae564ddcdac12b85f1b62
Omar Omar <oomar@umich.edu>
2021-06-10 08:42:57 -0400

    📦 Add Info Server Command

--- src/Commands/ServerCommands.ts
+++ src/Commands/ServerCommands.ts
@@ -2,7 +2,7 @@ import { Message } from 'https://deno.land/x/discordeno@10.5.0/mod.ts';
 import { CommandMap, Command } from '../Interfaces/Command.ts';
 import { GuildActivityModel, GuildModel } from '../Database/index.ts';
 import { IGuildActivity, IGuild } from '../Interfaces/Database.ts';
-
+import Config from '../config.ts';
 
 /**
  * Prints Command Interaction of users within the server
@@ -46,10 +46,37 @@ async function command_server_interaction(msg: Message, cmd: Command): Promise<a
   })
 }
 
+/**
+ * Prints Bot Information
+ * @param msg Message Object
+ * @param cmd Parsed Command Object
+ */
+ async function command_info(msg: Message, cmd: Command): Promise<any> {
+  // Get Total Guilds Stored
+  const totalGuilds = (await GuildModel.count()) || 0;
+  
+  return msg.send({
+    embed: {
+      title: 'Information',
+      description: `Discord Statistics Bot that monitors user statistics on a server, with the end goal of a personalized User and Server Statistics Experience much like Spotify's Wrapped.
+      Follow Development at: https://github.com/Ciaxur/DiscordStat.bot
+      Support the Developers by donating. Use \`!donate\` for more info.
+
+      This bot is currently in **${totalGuilds} Server${totalGuilds > 1 ? 's' : ''}**`,
+      footer: {
+        text: `Version ${Config.version}`,
+      },
+    },
+  });
+}
 
 export const SERVER_COMMANDS: CommandMap = {
   'server-interaction': {
     exec: command_server_interaction,
     description: 'Prints the current server\'s user interaction with this bot',
+  },
+  'info': {
+    exec: command_info,
+    description: 'Prints bot information',
   },
 };
