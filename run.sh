898fac39aef9941f0951248a33bb2ac88385c3d8
Omar Omar <oomar@umich.edu>
2021-07-02 21:44:35 -0400

    ♨️ Add containerized deno run

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 cd $(dirname $0)
-deno run --allow-read --allow-write --allow-net --unstable ./src/main.ts
+
+docker run --rm \
+  --net host \
+  -v "$(pwd):/app" \
+  -w "/app" \
+  -l deno_discordBot \
+  denoland/deno:alpine-1.11.3 \
+  deno run --allow-read --allow-write --allow-net --unstable src/main.ts
