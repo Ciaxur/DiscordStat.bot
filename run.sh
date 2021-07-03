d1a5d5b7e6a0924abb2b826ee705c06f95e5c32a
Omar Omar <oomar@umich.edu>
2021-07-02 22:10:31 -0400

    Modify run script

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ docker run --rm \
   -v "$(pwd):/app" \
   -w "/app" \
   -l deno_discordBot \
+  "$@" \
   denoland/deno:alpine-1.11.3 \
   deno run --allow-read --allow-write --allow-net --unstable src/main.ts
